53236531cf202a44d8820062e02005bc81197a34
zd4rova <fakenotbad75@gmail.com>
2023-12-30 13:32:03 +0300

    update: code refactoring

---
+++ cmd/app/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+	"github.com/zd4r/blur-collection-holders/internal/clients/blur"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("blur-collection-holders")
+	w.SetMaster()
+
+	collectionAddress := widget.NewEntry()
+	collectionAddress.SetPlaceHolder("collection address")
+
+	var (
+		ownerships blur.CollectionOwnerships
+		err        error
+	)
+
+	blurClient, err := blur.NewClient()
+	if err != nil {
+		log.Fatalf("failed to create blur client: %s", err.Error())
+	}
+
+	showHoldersTableButton := widget.NewButton(
+		"show",
+		func() {
+			tableWindow := a.NewWindow(fmt.Sprintf("%s holders", collectionAddress.Text))
+
+			ownerships, err = blurClient.GetCollectionOwnerships(collectionAddress.Text)
+			if err != nil {
+				if err != nil {
+					dialog.ShowInformation(
+						"error occurred",
+						err.Error(),
+						w,
+					)
+					return
+				}
+			}
+
+			tableWindow.SetContent(
+				container.NewBorder(
+					widget.NewButton(
+						"refresh",
+						func() {
+							ownerships, err = blurClient.GetCollectionOwnerships(collectionAddress.Text)
+							if err != nil {
+								dialog.ShowInformation(
+									"error occurred",
+									err.Error(),
+									w,
+								)
+								return
+							}
+						},
+					),
+					nil, nil, nil,
+					widget.NewList(
+						func() int {
+							return len(ownerships.Ownerships)
+						},
+						func() fyne.CanvasObject {
+							return container.NewBorder(
+								nil, nil,
+								widget.NewLabel(""),
+								widget.NewLabel(""),
+							)
+						},
+						func(id widget.ListItemID, o fyne.CanvasObject) {
+							c := o.(*fyne.Container)
+							l1 := c.Objects[0].(*widget.Label)
+							l1.SetText(ownerships.Ownerships[id].OwnerAddress)
+
+							l2 := c.Objects[1].(*widget.Label)
+							l2.SetText(strconv.Itoa(ownerships.Ownerships[id].NumberOwned))
+						}),
+				),
+			)
+
+			tableWindow.Show()
+		},
+	)
+
+	w.SetContent(container.New(
+		layout.NewGridLayout(2),
+		collectionAddress,
+		showHoldersTableButton,
+	))
+
+	w.ShowAndRun()
+}
---
+++ internal/clients/blur/collection_ownerships.go
@@ -0,0 +1,77 @@
+package blur
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	http "github.com/bogdanfinn/fhttp"
+	tlsClient "github.com/bogdanfinn/tls-client"
+	"github.com/bogdanfinn/tls-client/profiles"
+)
+
+const (
+	blurCollectionOwnershipsURL = "https://core-api.prod.blur.io/v1/collections/%s/ownerships"
+)
+
+type Client struct {
+	tlsClient.HttpClient
+}
+
+func NewClient() (*Client, error) {
+	options := []tlsClient.HttpClientOption{
+		tlsClient.WithTimeoutSeconds(30),
+		tlsClient.WithClientProfile(profiles.Chrome_105),
+		tlsClient.WithNotFollowRedirects(),
+	}
+
+	client, err := tlsClient.NewHttpClient(tlsClient.NewNoopLogger(), options...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create custom tls http client: %w", err)
+	}
+
+	return &Client{
+		client,
+	}, err
+}
+
+type CollectionOwnerships struct {
+	Success    bool `json:"success"`
+	Ownerships []struct {
+		OwnerAddress string `json:"ownerAddress"`
+		NumberOwned  int    `json:"numberOwned"`
+	} `json:"ownerships"`
+}
+
+func (c *Client) GetCollectionOwnerships(collectionAddress string) (CollectionOwnerships, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(blurCollectionOwnershipsURL, collectionAddress), nil)
+	if err != nil {
+		return CollectionOwnerships{}, err
+	}
+
+	req.Header = http.Header{
+		"user-agent": {"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"},
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return CollectionOwnerships{}, err
+	}
+
+	defer resp.Body.Close()
+
+	log.Println(fmt.Sprintf("status code: %d", resp.StatusCode))
+
+	readBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return CollectionOwnerships{}, err
+	}
+
+	data := new(CollectionOwnerships)
+	if err := json.Unmarshal(readBytes, data); err != nil {
+		return CollectionOwnerships{}, err
+	}
+
+	return *data, nil
+}
